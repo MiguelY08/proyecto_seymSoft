3481f3cd7836eae7566da80995263bdf72ba7a9a
MiguelY08 <yepesangel8@gmail.com>
2026-02-10 10:32:39 -0500

    Instalacion dependencias de logos y rutas

---
+++ frontend/src/app/router/appRouter.jsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from 'react-router-dom'
+import sidebar from '../router/appRouter.jsx';
+const appRouter = () => {
+  return (
+    <div>
+      {/* <Routes>
+        <Route element={}>
+          <Route path="/" element={<sidebar/>} />
+        </Route>
+      </Routes> */}
+      
+    </div>
+  )
+}
+
+export default appRouter
