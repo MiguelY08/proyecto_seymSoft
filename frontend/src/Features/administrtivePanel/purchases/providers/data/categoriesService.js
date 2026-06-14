e0b38534bc4cb18a5d7cd6eae66f13d3ef20fbd0
s3b4st1an-b3 <valenciasebas129@gmail.com>
2026-06-13 19:31:31 -0500

    aplico el apiClient

--- frontend/src/Features/administrtivePanel/purchases/providers/data/categoriesService.js
+++ frontend/src/Features/administrtivePanel/purchases/providers/data/categoriesService.js
@@ -1,20 +1,8 @@
-import axios from 'axios';
-
-const API_BASE_URL = 'http://localhost:3000/api/categories';
-
-const getAuthToken = () => {
-  return localStorage.getItem('accessToken') || '';
-};
+import apiClient from '../../../settings/apiClient.js';
 
 export const categoriesService = {
   getAll: async () => {
-    const response = await axios.get(API_BASE_URL, {
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${getAuthToken()}`
-      }
-    });
-    
+    const response = await apiClient.get('/categories');
     return response.data;
   }
 };
